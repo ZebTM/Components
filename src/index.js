69b04c42cb5659501b3adbce9458f6ac439b889d
Zachary Brown <zacharybrown@tinyhomeconsultingllc.com>
2021-08-11 13:48:54 -0400

    Updates

--- src/index.js
+++ src/index.js
@@ -7,22 +7,36 @@ import ApprovalCard from './ApprovalCard';
 const App = () => {
     return (
         <div className = "ui container comments">
-            <ApprovalCard />
-            <CommentDetail 
-                author = "Sam" 
-                timeAgo = "Today at 10:56PM" 
-                text = "Great Blog!" 
-                imageAvatar = {faker.image.avatar()}/>
-            <CommentDetail 
-                author = "Alex" 
-                timeAgo = "Today at 3:04AM" 
-                text = "Trash blog!" 
-                imageAvatar = {faker.image.avatar()}/>
-            <CommentDetail 
-                author = "Jane" 
-                timeAgo = "Today at 4:46PM" 
-                text = "-_-" 
-                imageAvatar = {faker.image.avatar()}/>
+            <ApprovalCard>
+                <div>
+                    <h4>Warning!</h4>
+                Are you sure you want to do this?
+                </div>
+            </ApprovalCard>
+            <ApprovalCard> 
+                <CommentDetail 
+                    author = "Sam" 
+                    timeAgo = "Today at 10:56PM" 
+                    text = "Great Blog!" 
+                    imageAvatar = {faker.image.avatar()}
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail 
+                    author = "Alex" 
+                    timeAgo = "Today at 3:04AM" 
+                    text = "Trash blog!" 
+                    imageAvatar = {faker.image.avatar()}
+                />
+            </ApprovalCard>
+            <ApprovalCard>    
+                <CommentDetail 
+                    author = "Jane" 
+                    timeAgo = "Today at 4:46PM" 
+                    text = "-_-" 
+                    imageAvatar = {faker.image.avatar()}
+                />
+            </ApprovalCard>    
         </div>
     );
 };
