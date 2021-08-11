1a3e45e48cccee7fce5b9336e87ddc19cbbeefbc
Zachary Brown <zacharybrown@tinyhomeconsultingllc.com>
2021-08-11 11:08:50 -0400

    Added faker

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
 
 const App = () => {
     return (
         <div className = "ui container comments">
             <div className="comment">
                 <a href="/" className="avatar">
-                    <img alt="avatar" />    
+                    <img alt="avatar" src={faker.image.avatar} />    
                 </a>
                 <div className = "content">
                     <a href="/" className = "author">
@@ -15,7 +16,7 @@ const App = () => {
                     <div className="metadata">
                         <span className = "date"> today at 6:00PM</span>
                     </div>
-                        <div className='text'>Nice blog post!</div>
+                        <div className="text">Nice blog post!</div>
                 </div>
             </div>
         </div>
