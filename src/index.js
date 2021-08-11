8a7b704fbddfb048c94dcce357fd3242e72febe5
Zachary Brown <zacharybrown@tinyhomeconsultingllc.com>
2021-08-11 13:07:05 -0400

    New approval card added

--- src/index.js
+++ src/index.js
@@ -2,13 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
     return (
         <div className = "ui container comments">
-            <CommentDetail />
-            <CommentDetail />
-            <CommentDetail />
+            <ApprovalCard />
+            <CommentDetail 
+                author = "Sam" 
+                timeAgo = "Today at 10:56PM" 
+                text = "Great Blog!" 
+                imageAvatar = {faker.image.avatar()}/>
+            <CommentDetail 
+                author = "Alex" 
+                timeAgo = "Today at 3:04AM" 
+                text = "Trash blog!" 
+                imageAvatar = {faker.image.avatar()}/>
+            <CommentDetail 
+                author = "Jane" 
+                timeAgo = "Today at 4:46PM" 
+                text = "-_-" 
+                imageAvatar = {faker.image.avatar()}/>
         </div>
     );
 };
